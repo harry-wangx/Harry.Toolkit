b2d5b6ee7faf41115ddbf2c0cd3ffa4350ca560b
Harry <gamelong@qq.com>
2016-05-30 15:53:39 +0800

    ~

--- src/Core/Harry.Common/Common/Utils.cs
+++ src/Core/Harry.Common/Common/Utils.cs
@@ -7,7 +7,6 @@ namespace Harry.Common
 {
     public static class Utils
     {
-        private static DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         /// <summary>
         /// 获取时间戳
@@ -15,7 +14,7 @@ namespace Harry.Common
         /// <returns></returns>
         public static long GetTimeStamp()
         {
-            return (long)((DateTime.UtcNow - Jan1st1970).TotalMilliseconds);
+            return DateTime.UtcNow.ToTimeStamp();
         }
     }
 }
