7894df434da1cd58642fed18ae8a3d2771b82044
Harry <harry_w@foxmail.com>
2016-06-05 14:20:28 +0800

    ~

---
+++ src/Core/Harry.Common/Entity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Harry
+{
+    public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    {
+        public TId Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var entity = obj as Entity<TId>;
+            if (entity != null)
+            {
+                return this.Equals(entity);
+            }
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+
+        public bool Equals(Entity<TId> other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return this.Id.Equals(other.Id);
+        }
+    }
+}
