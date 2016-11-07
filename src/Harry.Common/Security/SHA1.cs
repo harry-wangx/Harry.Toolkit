91f33f502425e689040fcf1e3165059ecbee0bde
Harry <harry_w@foxmail.com>
2016-11-07 15:03:36 +0800

    优化SHA1方法,StringBuilder的构造函数中,capacity参数由32改为40

--- src/Harry.Common/Security/SHA1.cs
+++ src/Harry.Common/Security/SHA1.cs
@@ -11,50 +11,55 @@ namespace Harry.Security
 #else
         static System.Security.Cryptography.SHA1 hasher = System.Security.Cryptography.SHA1.Create();
 #endif
-        ///// <summary>
-        ///// SHA1加密
-        ///// </summary>
-        ///// <param name="input">输入字符串</param>
-        ///// <param name="encoding">字符编码</param>
-        ///// <returns></returns>
-        //public static string ComputeHash(string input, Encoding encoding)
-        //{
-        //    if (!Common.Utils.HasValue(input))
-        //    {
-        //        throw new ArgumentNullException(nameof(input));
-        //    }
+        /// <summary>
+        /// SHA1加密
+        /// </summary>
+        /// <param name="input">输入字符串</param>
+        /// <param name="encoding">字符编码</param>
+        /// <returns>返回大写SHA1值</returns>
+        public static string ComputeHash(string input, Encoding encoding)
+        {
+            if (!Common.Utils.HasValue(input))
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
 
-        //    byte[] t = hasher.ComputeHash(encoding.GetBytes(input));
+            byte[] t =
+#if !NET20 && !NET35
+                hasher.Value.ComputeHash(encoding.GetBytes(input));
+#else
+                hasher.ComputeHash(encoding.GetBytes(input));
+#endif
 
-        //    StringBuilder sb = new StringBuilder(32);
+            StringBuilder sb = new StringBuilder(40);
 
-        //    for (int i = 0; i < t.Length; i++)
-        //    {
-        //        sb.Append(t[i].ToString("X").PadLeft(2, '0'));
-        //    }
+            for (int i = 0; i < t.Length; i++)
+            {
+                sb.Append(t[i].ToString("X").PadLeft(2, '0'));
+            }
 
-        //    return sb.ToString();
-        //}
+            return sb.ToString();
+        }
 
-        ///// <summary>
-        ///// SHA1加密
-        ///// </summary>
-        ///// <param name="input">输入字符串</param>
-        ///// <param name="charset">字符集</param>
-        ///// <returns></returns>
-        //public static string ComputeHash(string input, string charset)
-        //{
-        //    return ComputeHash(input, Encoding.GetEncoding(charset));
-        //}
+        /// <summary>
+        /// SHA1加密
+        /// </summary>
+        /// <param name="input">输入字符串</param>
+        /// <param name="charset">字符集</param>
+        /// <returns></returns>
+        public static string ComputeHash(string input, string charset)
+        {
+            return ComputeHash(input, Encoding.GetEncoding(charset));
+        }
 
-        ///// <summary>
-        ///// SHA1加密(默认使用UTF8)
-        ///// </summary>
-        ///// <param name="input">输入字符串</param>
-        ///// <returns></returns>
-        //public static string ComputeHash(string input)
-        //{
-        //    return ComputeHash(input, Encoding.UTF8);
-        //}
+        /// <summary>
+        /// SHA1加密(默认使用UTF8)
+        /// </summary>
+        /// <param name="input">输入字符串</param>
+        /// <returns></returns>
+        public static string ComputeHash(string input)
+        {
+            return ComputeHash(input, Encoding.UTF8);
+        }
     }
 }
