42c6fe7870fa76985d530710f078f4eca4818607
Harry <harry_w@foxmail.com>
2017-05-09 13:48:02 +0800

    增加task的扩展

---
+++ src/Harry.Common/Extensions/TaskExtensions.cs
@@ -0,0 +1,24 @@
+﻿#if !NET35
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Harry.Extensions
+{
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// 调用异步方法时,不提示未使用await错误
+        /// </summary>
+        /// <param name="task"></param>
+#if !NET40
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static void NoWarning(this Task task) { }
+    }
+}
+
+#endif
