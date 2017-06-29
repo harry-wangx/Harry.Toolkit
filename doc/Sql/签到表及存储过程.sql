69be6122adc8b2e84ac9985c5b14b4115dd5033b
Harry <harry_w@foxmail.com>
2017-06-29 18:26:27 +0800

    又搞了个版本的签到表Sql

--- doc/Sql/签到表及存储过程.sql
+++ doc/Sql/签到表及存储过程.sql
@@ -31,43 +31,115 @@ with(FILLFACTOR=80)
 go
 
 
---签到
+----签到方法一
+--IF OBJECT_ID (N'dbo.up_qiandao_add', N'P') IS NOT NULL
+--	DROP proc dbo.up_qiandao_add;
+--GO
+--create proc dbo.up_qiandao_add
+--@PromotionId int,
+--@UserId varchar(50),
+--@MaxCount int =30
+--as 
+--begin
+--	declare @now datetime = GETDATE() --当前时间 cast('2017-7-3' as datetime) -- 
+--	declare @dayStamp int --时间戳(精确到日)
+--	declare @result int --返回结果,1为签到成功,-1为已签到,-2为达到最大签到次数,-1000为重复插入签到数据
+--	declare @counter int --新的签到计数
+--	declare @continuousCounter int --新的连续签到计数
+
+--	set @dayStamp=DATEDIFF(dd,'1970-1-1',@now)
+	
+--	if(select COUNT(1) from dbo.QianDao with(nolock) where 
+--		PromotionId=@PromotionId 
+--		and DayStamp=@dayStamp
+--		and UserId=@UserId) >0
+--		set @result=-1 --当天已经签到,直接返回
+--	else if(select COUNT(1) from dbo.QianDao with(nolock) where 
+--		PromotionId=@PromotionId 
+--		and UserId=@UserId) >=@MaxCount
+--		set @result=-2 --已达最大签到次数,直接返回
+--	else
+--		begin
+--			declare @lastDayStamp int --最后一次签到时间戳
+--			declare @lastCounter int --最后一次签到计数
+--			declare @lastContinuousCounter int --最后一次连续签到计数
+			
+--			select top 1  @lastDayStamp=DayStamp,@lastCounter=[Counter],@lastContinuousCounter=ContinuousCounter
+--			  from dbo.QianDao where 
+--				PromotionId=@PromotionId 
+--				and DayStamp<@dayStamp
+--				and UserId=@UserId
+--				order by Id desc
+			
+--			set @counter=ISNULL(@lastCounter,0)+1
+--			set @lastDayStamp=ISNULL(@lastDayStamp,0)
+--			if(@dayStamp-@lastDayStamp>1)
+--				set @continuousCounter=1 --说明是非连续签到
+--			else
+--				begin
+--					set @continuousCounter=ISNULL(@lastContinuousCounter,0)+1
+--				end
+				
+--			begin try
+--				insert into dbo.QianDao(PromotionId,UserId,DayStamp,[Counter],ContinuousCounter,CreateTime)
+--				values(@PromotionId,@UserId,@dayStamp,@counter,@continuousCounter,@now)
+--				set @result=1
+--			end try
+--			begin catch
+--				set @result=-1000
+--				set @counter=null
+--				set @continuousCounter=null
+--			end catch 
+
+--		end
+--	--返回相关信息
+--	select @result as Result,
+--	@userId as UserId,
+--	@counter as [Counter],
+--	@continuousCounter as ContinuousCounter,
+--	@now as CreateTime,
+--	CAST(SCOPE_IDENTITY() AS bigint) AS Id
+--end
+--go
+
+--签到方法二
 IF OBJECT_ID (N'dbo.up_qiandao_add', N'P') IS NOT NULL
 	DROP proc dbo.up_qiandao_add;
 GO
 create proc dbo.up_qiandao_add
 @PromotionId int,
-@UserId varchar(50)
+@UserId varchar(50),
+@MaxCount int =30
 as 
 begin
-	declare @now datetime =  GETDATE() --当前时间 cast('2017-7-3' as datetime) -- 
+	declare @now datetime = GETDATE() --当前时间 cast('2017-7-3' as datetime) -- 
 	declare @dayStamp int --时间戳(精确到日)
-	declare @result int --返回结果,1为签到成功,0为已签到,-1为错误
+	declare @result int --返回结果,1为签到成功,-1为已签到,-2为达到最大签到次数,-1000为重复插入签到数据
 	declare @counter int --新的签到计数
 	declare @continuousCounter int --新的连续签到计数
+	declare @lastDayStamp int --最后一次签到时间戳
+	declare @lastCounter int --最后一次签到计数
+	declare @lastContinuousCounter int --最后一次连续签到计数
 
 	set @dayStamp=DATEDIFF(dd,'1970-1-1',@now)
 	
-	if(select COUNT(1) from dbo.QianDao with(nolock) where 
+	select top 1  @lastDayStamp=DayStamp,@lastCounter=[Counter],@lastContinuousCounter=ContinuousCounter
+	  from dbo.QianDao with(nolock) where 
 		PromotionId=@PromotionId 
-		and DayStamp=@dayStamp
-		and UserId=@UserId) >0
-		set @result=0 --当天已经签到,直接返回
+		and UserId=@UserId
+		order by Id desc
+		
+	set @lastDayStamp=ISNULL(@lastDayStamp,0)
+	set @lastCounter=ISNULL(@lastCounter,0)
+	
+	if(@lastDayStamp>=@dayStamp)
+		set @result=-1 --当天已经签到,直接返回
+	else if(@lastCounter>=@MaxCount)
+		set @result=-2 --已达最大签到次数,直接返回
 	else
 		begin
-			declare @lastDayStamp int --最后一次签到时间戳
-			declare @lastCounter int --最后一次签到计数
-			declare @lastContinuousCounter int --最后一次连续签到计数
+			set @counter=@lastCounter+1
 			
-			select top 1  @lastDayStamp=DayStamp,@lastCounter=[Counter],@lastContinuousCounter=ContinuousCounter
-			  from dbo.QianDao where 
-				PromotionId=@PromotionId 
-				and DayStamp<@dayStamp
-				and UserId=@UserId
-				order by Id desc
-			
-			set @counter=ISNULL(@lastCounter,0)+1
-			set @lastDayStamp=ISNULL(@lastDayStamp,0)
 			if(@dayStamp-@lastDayStamp>1)
 				set @continuousCounter=1--说明非连续签到
 			else
@@ -81,7 +153,7 @@ begin
 				set @result=1
 			end try
 			begin catch
-				set @result=-1
+				set @result=-1000
 				set @counter=null
 				set @continuousCounter=null
 			end catch 
@@ -107,13 +179,13 @@ create proc dbo.up_qiandao_get_last
 @UserId varchar(50)
 as 
 begin 
-select top 1 * from dbo.QianDao where 
+select top 1 * from dbo.QianDao with(nolock) where 
 				PromotionId=@PromotionId 
 				and UserId=@UserId
 				order by Id desc
 end
 
 
---execute dbo.up_qiandao_add 1,'abc'
+--execute dbo.up_qiandao_add 1,'abc',1
 --execute dbo.up_qiandao_get_last 1,'abc'
 
---
+++ doc/Sql/签到表及存储过程2.sql
@@ -0,0 +1,129 @@
+use TestDb;
+go
+
+IF OBJECT_ID (N'dbo.QianDao', N'U') IS NOT NULL
+	DROP TABLE dbo.QianDao;
+GO
+
+create table dbo.QianDao
+(
+	Id bigint identity(1,1) primary key,
+	
+	PromotionId int not null default(0), --活动编号,
+	UserId varchar(50) not null, --用户编号
+	DayStamp int not null, --时间戳(精确到天).每活动/每用户/时间戳,只能有一条记录.
+	
+	[Counter] int not null,--当前活动签到数量
+	
+	Tag varchar(50) null , --自定义标记
+	Row_Version int not null default(0), --版本号.需要处理并发操作时用.
+	CreateTime datetime not null default(getdate())
+)
+go
+
+--创建索引
+create unique  nonclustered index IX_QianDao
+on dbo.QianDao(
+	DayStamp asc,
+	PromotionId asc,
+	UserId asc
+)
+include([Counter])
+with(FILLFACTOR=80)
+go
+
+
+--签到
+IF OBJECT_ID (N'dbo.up_qiandao_add', N'P') IS NOT NULL
+	DROP proc dbo.up_qiandao_add;
+GO
+create proc dbo.up_qiandao_add
+@PromotionId int, --活动编号
+@UserId varchar(50), --用户编号
+@Type int =1, --签到类型,1为累计签到,2为连续签到
+@MaxCount int =30,--最大签到次数
+@AutoStart bit=0 --签到完成后,是否自动新一轮签到
+as 
+begin
+	if(@Type<>1 and @Type<>2)
+		RAISERROR ('Parameter ''Type'' must be 1 or 2.' , 16, 1) WITH NOWAIT
+
+	declare @now datetime = GETDATE() --当前时间 cast('2017-7-3' as datetime) -- 
+	declare @dayStamp int --时间戳(精确到日)
+	declare @result int --返回结果,1为签到成功,0为已签到,-1为错误
+	declare @counter int --新的签到计数
+	declare @lastDayStamp int --最后一次签到时间戳
+	declare @lastCounter int --最后一次签到计数
+
+	set @dayStamp=DATEDIFF(dd,'1970-1-1',@now)
+	
+	select top 1  @lastDayStamp=DayStamp,@lastCounter=[Counter]
+	  from dbo.QianDao with(nolock) where 
+		PromotionId=@PromotionId 
+		and UserId=@UserId
+		order by Id desc
+		
+	set @lastDayStamp=ISNULL(@lastDayStamp,0)
+	set @lastCounter=ISNULL(@lastCounter,0)
+	
+	if(@lastDayStamp>=@dayStamp)
+		set @result=-1 --当天已经签到,直接返回
+	else if( @AutoStart=0 and @lastCounter>=@MaxCount)
+		set @result=-2 --已达最大签到次数,直接返回
+	else
+		begin
+			if(@lastCounter>=@MaxCount)
+				set @counter=1
+			else
+			begin
+				if(@Type=1)
+					set @counter=@lastCounter+1 --累计签到
+				else
+				begin
+					if(@dayStamp-@lastDayStamp>1)
+						set @counter=1     --说明非连续签到
+					else
+						set @counter=@lastCounter+1				
+				end
+			end
+				
+			begin try
+				insert into dbo.QianDao(PromotionId,UserId,DayStamp,[Counter],CreateTime,Row_Version)
+				values(@PromotionId,@UserId,@dayStamp,@counter,@now,0)
+				set @result=1
+			end try
+			begin catch
+				set @result=-1000
+				set @counter=null
+			end catch 
+
+		end
+	--返回相关信息
+	select @result as Result,
+	@userId as UserId,
+	@counter as [Counter],
+	@now as CreateTime,
+	CAST(SCOPE_IDENTITY() AS bigint) AS Id
+end
+go
+
+
+--获取最后一次签到记录
+IF OBJECT_ID (N'dbo.up_qiandao_get_last', N'P') IS NOT NULL
+	DROP proc dbo.up_qiandao_get_last;
+GO
+create proc dbo.up_qiandao_get_last
+@PromotionId int,
+@UserId varchar(50)
+as 
+begin 
+select top 1 * from dbo.QianDao with(nolock) where 
+				PromotionId=@PromotionId 
+				and UserId=@UserId
+				order by Id desc
+end
+
+
+--execute dbo.up_qiandao_add 1,'abc',2,3,1
+--execute dbo.up_qiandao_get_last 1,'abc'
+
