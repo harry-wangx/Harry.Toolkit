95b0daefa8397e684e20ce181f287246efaadaf5
Harry <harry_w@foxmail.com>
2017-08-11 18:03:37 +0800

    .

---
+++ src/Harry.Common/BitArray.cs
@@ -0,0 +1,60 @@
+﻿/*
+ 来源于<NET CLR via c# 4> 第10章 10.2有参数性
+ */
+using System;
+
+namespace Harry
+{
+    public sealed class BitArray
+    {
+        //容纳了二进制位的私有字节数组
+        private byte[] m_byteArray;
+        private int m_numBits;
+
+        //下面的构造器用于分配字节数组,并将所有位设为0
+        public BitArray(int numBits)
+        {
+            //先验证实参
+            if (numBits <= 0)
+                throw new ArgumentOutOfRangeException(nameof(numBits));
+
+            //保存位的个数
+            m_numBits = numBits;
+            //为位数组分配字节
+            m_byteArray = new byte[(numBits + 7) / 8];
+        }
+
+        //下面是索引器(有参属性)
+        public bool this[int bitPos]
+        {
+            //下面是索引器的get访问器方法
+            get
+            {
+                //先验证实参
+                if ((bitPos < 0) || (bitPos >= m_numBits))
+                    throw new ArgumentOutOfRangeException(nameof(bitPos));
+
+                //返回指定索引处的位的状态
+                return (m_byteArray[bitPos / 8] & (1 << (bitPos % 8))) != 0;
+            }
+
+            //下面是索引器的set访问器方法
+            set
+            {
+                if ((bitPos < 0) || (bitPos >= m_numBits))
+                    throw new ArgumentOutOfRangeException(nameof(bitPos), bitPos.ToString());
+
+                if (value)
+                {
+                    //将指定索引处的位设为true
+                    m_byteArray[bitPos / 8] = (byte)(m_byteArray[bitPos / 8] | (1 << (bitPos % 8)));
+                }
+                else
+                {
+                    //将指定索引处的位设为false
+                    m_byteArray[bitPos / 8] = (byte)(m_byteArray[bitPos / 8] & ~(1 << (bitPos % 8)));
+                }
+            }
+        }
+    }
+}
---
+++ src/Harry.Toolkit/Common/OperationTimer.cs
@@ -0,0 +1,39 @@
+﻿/*来自<NET CLR via c# 4 > 第12章 泛型*/
+using System;
+using System.Diagnostics;
+
+namespace Harry.Common
+{
+    /// <summary>
+    /// 测试代码执行性能用
+    /// </summary>
+    public sealed class OperationTimer : IDisposable
+    {
+        private Stopwatch m_stopwatch;
+        private string m_text;
+        private int m_collectionCount;
+
+        public OperationTimer(string text)
+        {
+            PrepareForOperation();
+
+            m_text = text;
+            m_collectionCount = GC.CollectionCount(0);
+
+            //这应该是方法的最后一个语句,从而最大程度保证计时的准确性
+            m_stopwatch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{m_stopwatch.Elapsed} (GCs={GC.CollectionCount(0) - m_collectionCount})  {m_text}");
+        }
+
+        private static void PrepareForOperation()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
